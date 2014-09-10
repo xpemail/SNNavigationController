cf0322907ae84ebd23e91e5fa95a08e1d9a66c9a
wuxiande <wuxiande@soooner.com>
2014-09-10 16:35:25 +0800

    Release 1.0.0.

---
+++ UINavigationController+Extra.h
@@ -0,0 +1,52 @@
+//
+//  UINavigationController+Custom.h 
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UINavigationController(Navigation)
+
+- (NSArray *)popToViewControllerClass:(Class)aClass animated:(BOOL)animated;
+
+
+@end
+
+@interface UINavigationItem(Extra)
+
+-(void)setIndicatorImage:(UIImage *)indicatorImage;
+
+-(UIImage *)indicatorImage;
+ 
+-(void)setIndicatorHighlightedImage:(UIImage *)indicatorHighlightedImage;
+
+-(UIImage *)indicatorHighlightedImage;
+
+- (void)setLeftBarButtonTitle:(NSString *)title target:(id)target action:(SEL)action indicator:(BOOL)indicator;
+
+- (void)setLeftBarButtonImage:(UIImage *)image target:(id)target action:(SEL)action;
+
+- (void)setLeftBarButton:(UIButton *)button;
+
+- (void)setRightBarButtonTitle:(NSString *)title target:(id)target action:(SEL)action;
+
+- (void)setRightBarButtonImage:(UIImage *)image target:(id)target action:(SEL)action;
+
+- (void)setRightBarButton:(UIButton *)button;
+
+
+
+-(void)setRightBarButtons:(NSArray*)array target:(id)target action:(SEL)action;
+
+-(void)setMidTitleView:(UIView*)image;
+
+@end
+
+@interface UINavigationBar (Extra)
+
+-(void)setBackgroundImage:(UIImage *)image;
+
+-(void)setTitleColor:(UIColor *)color;
+
+-(void)setTitleColor:(UIColor *)color font:(UIFont *)font titleShadowColor:(UIColor *)shadowColor shadowOffset:(CGSize)shadowOffset;
+
+@end
